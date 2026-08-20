f6eb4dbf12e717d70a6549227c1d740209c46cbb
rico <r03292760@gmail.com>
2026-08-20 14:21:21 +0200

    chore: wire stability guards into resource

--- rs-jobscreator/fxmanifest.lua
+++ rs-jobscreator/fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'rs-jobscreator'
 author 'RS Development'
 description 'RS Jobs Creator - ESX + oxmysql + ox_lib + ox_inventory'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -18,8 +18,10 @@ client_scripts {
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
+    'server/bootstrap.lua',
     'server/scanner.lua',
-    'server/main.lua'
+    'server/main.lua',
+    'server/stability.lua'
 }
 
 ui_page 'html/index.html'
@@ -27,7 +29,8 @@ ui_page 'html/index.html'
 files {
     'html/index.html',
     'html/style.css',
-    'html/app.js'
+    'html/app.js',
+    'html/stability.js'
 }
 
 dependency 'es_extended'
